aad8d88f3d3f15c9fee6502c4dc8fc1da2ee3529
daneradev <daneradev@gmail.com>
2022-12-17 20:09:47 -0500

    fix: rest controller modified

--- src/main/java/com/danera/demo/controller/Controller.java
+++ src/main/java/com/danera/demo/controller/Controller.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class Controller {
 
-    @GetMapping
+    @GetMapping("/hi")
     public String getGreetings() {
         return "Hello buddy";
     }
